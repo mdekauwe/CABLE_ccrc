c6eec3d7a304f6f88d77e1525ccdf989b5636c98
mdekauwe <mdekauwe@gmail.com>
2017-09-14 17:59:55 +1000

    Added more generic build script

--- create_build_file.sh
+++ create_build_file.sh
@@ -9,13 +9,22 @@
 
 fname=build.ksh
 tag=CABLE-2.3.4
-branch="test"
-nc_path="/usr/local/netcdf/intel/4.1.3"
-fc="ifort"
+branch="tag"
 HOST_MACH=`uname -n | cut -c 1-4`
+ARCH=$(uname -s)
 
-module add intel/15.0.3.187
-module add netcdf/4.2.1-intel
+if [ $ARCH == "Linux" ]
+then
+    module add intel/15.0.3.187
+    module add netcdf/4.2.1-intel
+    SED=sed
+    nc_path="/usr/local/netcdf/intel/4.1.3"
+    fc="ifort"
+else
+    nc_path="/opt/local"
+    fc="gfortran"
+    SED=gsed
+fi
 
 cd $tag"_"$branch/offline
 
@@ -27,7 +36,7 @@ gawk -v var="$HOST_MACH" '/   set/ {$0=$0" "var} 1' $fname > x.tmp
 mv x.tmp $fname
 
 # Step 2
-sed -i "8i\\
+$SED -i "8i\\
 host_$HOST_MACH(){\\
     export NCDIR='$nc_path/lib/'\\
     export NCMOD='$nc_path/include/'\\
