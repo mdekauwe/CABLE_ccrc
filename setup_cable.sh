e536feba084d8f9503e8c8bbd4a456c68508cd59
mdekauwe <mdekauwe@gmail.com>
2017-09-15 09:54:41 +1000

    Fixed AUX logic

--- setup_cable.sh
+++ setup_cable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Get latest CABLE tagged version setup on the CCRC storm servers
+# Get the CABLE code: check out a tag and make a new branch
 # -> https://trac.nci.org.au/svn/cable/tags/CABLE-2.3.4/
 #
 # author: Martin De Kauwe
@@ -22,10 +22,8 @@ fi
 svn copy $root/tags/$tag $root/branches/Users/$user/$tag"_"$branch -m "$msg"
 svn checkout $root/branches/Users/$user/$tag"_"$branch $tag"_"$branch
 
-if [ -d "../CABLE-AUX" ]
+if [ ! -d "../CABLE-AUX" ]
 then
-    echo "Directory exists"
-else
     svn checkout $root/branches/Share/CABLE-AUX CABLE-AUX
     if [ "$cleanup" = true ]
     then
