c24df6ad4cdc697c3ba3e8c4d4f9f3ae5eb2fe47
mdekauwe <mdekauwe@gmail.com>
2017-09-15 10:15:35 +1000

    Built more flexibility into the setup script, now does trunk, tag and other peoples branches

--- setup_cable.sh
+++ setup_cable.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #
-# Get the CABLE code: check out either the trunk, or a specific tag and make a
-# new branch
+# Get the CABLE code and make a branch
+#
+# Options to:
+#   - (i) check out the trunk
+#   - (ii) a specific tag
+#   - (iii)or someone else's branch
+#
 # -> https://trac.nci.org.au/svn/cable/tags/CABLE-2.3.4/
 #
 # author: Martin De Kauwe
@@ -10,21 +15,51 @@
 
 root="https://trac.nci.org.au/svn/cable"
 user="mgk576"
-tag="CABLE-2.3.4"
-branch="test"
 msg="\"setup test repo\""
 cleanup=true
+
+# Default behaviour is to get the latest tag (noting that this link will be out
+# of date). Tag
+tag="CABLE-2.3.4"
+branch="test"
+
+# if you change either of these options (both false by default), you need to
+# edit below in the if statements too
 trunk=false
+someones_branch=false
+
+# Get the trunk?
+if [ "$trunk" == true ]
+then
+    someones_branch=false
+    tag="CABLE_trunk"
+fi
+
+# Grab someone else's branch?
+if [ "$someones_branch" == true ]
+then
+    trunk=false
+    other_user="mrd561"
+    their_branch="CMIP6-GM2"
+    tag="CABLE-"$their_branch
+    branch="what"
+fi
+
 
 if [ ! .svn ]
 then
     rm -rf .svn
 fi
 
-if [ "$trunk" = true ]
+if [ "$trunk" == true ]
 then
-    svn copy $root/trunk $root/branches/Users/$user/$tag"_trunk" -m "$msg"
-    svn checkout $root/branches/Users/$user/$tag"_trunk" $tag"_trunk"
+    # NB tag here doesn't mean the tagged version! See above
+    svn copy $root/trunk $root/branches/Users/$user/$tag -m "$msg"
+    svn checkout $root/branches/Users/$user/$tag $tag
+elif [ "$someones_branch" == true ]
+then
+    svn copy $root/branches/Users/$other_user/$their_branch $root/branches/Users/$user/$tag"_"$branch -m "$msg"
+    svn checkout $root/branches/Users/$user/$tag"_"$branch $tag"_"$branch
 else
     svn copy $root/tags/$tag $root/branches/Users/$user/$tag"_"$branch -m "$msg"
     svn checkout $root/branches/Users/$user/$tag"_"$branch $tag"_"$branch
@@ -33,7 +68,7 @@ fi
 if [ ! -d "../CABLE-AUX" ]
 then
     svn checkout $root/branches/Share/CABLE-AUX CABLE-AUX
-    if [ "$cleanup" = true ]
+    if [ "$cleanup" == true ]
     then
         mv CABLE-AUX ../.
     fi
