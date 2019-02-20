51398f579972e86a2ed1fd729f85c289a91d9c91
mdekauwe <mdekauwe@gmail.com>
2019-02-21 10:06:39 +1100

    Added delete branch script

--- remove_branch.sh
+++ remove_branch.sh
@@ -12,4 +12,4 @@ root="https://trac.nci.org.au/svn/cable"
 user="mgk576"
 msg="\"deleting branch\""
 
-svn rm $root/branches/Users/$branch -m "$msg"
+svn rm $root/branches/Users/$user/$branch -m "$msg"
